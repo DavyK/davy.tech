c65b87f650aff6746d0f9ecccbb16615c6c8915f
Davy Kavanagh <davy.kavanagh@intercom.io>
2019-06-08 10:33:01 +0100

    all the changes

---
+++ src/js/Avatar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import avatar from '../../static/images/avatar.jpg';
+
+export default () => (
+  <img
+    className="avatar"
+    src={avatar}
+  />
+)
---
+++ src/js/timeline-data.js
@@ -0,0 +1,45 @@
+export default {
+  items: [{
+    fromDate: 'Sep, 2002',
+    toDate: 'May, 2006',
+    place: 'NUI Maynooth, Maynooth, Ireland',
+    title: 'BSc in Biology',
+    description: 'I went to university!'
+  },{
+    fromDate: 'Sep, 2008',
+    toDate: 'Sep, 2009',
+    place: "Queen's University, Belfast, United Kingdom",
+    title: 'MSc in Molecular Medicine',
+    description: 'I got a masters',
+  }, {
+    fromDate: 'Sep, 2009',
+    toDate: 'Jun, 2012',
+    place: "Queen's University, Belfast, United Kingdom",
+    title: 'PhD in Human Genetics and Bioinformatics',
+    description: "I'm a doctor",
+  }, {
+    fromDate: 'Jun, 2012',
+    toDate: 'Aug, 2014',
+    place: "Cardiff University, Cardiff, United Kingdom",
+    title: 'Research Fellow in Psychiatric Genetics',
+    description: "I'm also a data analyst",
+  }, {
+    fromDate: 'Sep, 2014',
+    toDate: 'Mar, 2016',
+    place: "School of Medicine at Mount Sinai, New York, USA",
+    title: 'Research Fellow in Psychiatric Genetics',
+    description: "I'm also a data analyst",
+  },{
+    fromDate: 'Apr, 2016',
+    toDate: 'Nov, 2018',
+    place: "Oxford Nanopore Technologies",
+    title: 'Fullstack developer',
+    description: "python, react, css, sass, docker, terraform, postgres, mongo",
+  }, {
+    fromDate: 'Nov, 2019',
+    toDate: 'Present',
+    place: "Intercom",
+    title: 'Product Engineer',
+    description: "product, ruby, rails, emberjs, product, css, sass, terraform, product",
+  }],
+};
