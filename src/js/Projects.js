925594a314862ecdd53f15c4ae0cdf71a368b274
Davy Kavanagh <davy.kavanagh@intercom.io>
2019-06-10 17:25:46 +0100

    add pyweek links

--- src/js/Projects.js
+++ src/js/Projects.js
@@ -9,6 +9,13 @@ const Projects = () => (
       <li>This site!</li>
       <li><Link to='/gol'>Conway's game of life</Link></li>
       <li><a href="https://github.com/DavyK/pygame-examples/">Maze building and path finding with python and pygame</a></li>
+      <li>
+        <p><a href="https://pyweek.org/">Pyweek</a> Entries:</p>
+        <ul>
+          <li><a href="https://pyweek.org/e/KA/">Destination Irrelevant</a></li>
+          <li><a href="https://pyweek.org/e/KAP/">Cultivate</a></li>
+        </ul>
+      </li>
     </ul>
   </React.Fragment>
 );
