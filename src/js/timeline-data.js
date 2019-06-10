2238be1e687899e292c8682b92c4dea068764b9c
Davy Kavanagh <davy.kavanagh@intercom.io>
2019-06-10 11:32:04 +0100

    update timeline

--- src/js/timeline-data.js
+++ src/js/timeline-data.js
@@ -3,14 +3,14 @@ export default {
     id: 'intercom',
     fromDate: 'Nov, 2019',
     toDate: 'Present',
-    place: "Intercom",
+    place: "Intercom, London, UK",
     title: 'Product Engineer',
     description: "Designing and building product features across the backend and frontend. Responsibilities included designing and implementing technical solutions to deliver impactful features, communicating with customers to identify problems to be solved, and managing the roll out of large features for thousands for customers. Technologies used: Ruby (rails), Javascript (Vanilla, Ember JS, React), databases (Mysql, Mongo, Aws Redshift, Elasticsearch, Redis). Other tools: Git, Github, Python (for data analysis), Docker, docker-compose, data-dog, honeycomb.",
   },{
     id: 'nanopore',
     fromDate: 'Apr, 2016',
     toDate: 'Nov, 2018',
-    place: "Oxford Nanopore Technologies",
+    place: "Oxford Nanopore Technologies, Oxford, UK",
     title: 'Fullstack developer',
     description: "Full stack development of several different data tracking and reporting micro services. Responsible for deploying and maintaining several different APIs and frontend applications to capture and report on diverse data sources. Micro services are maintained and deployed using a continuous integration strategy. Technologies used: Python (django, flask), Javascript (Node, React, Redux), Uwsgi, Nginx, databases (MySQL, Postgresql, MongoDB, Redis, Elasticsearch), Other tools: Celery, Docker, Docker-compose, Docker-swarm, Git, GitLab CI, Kibana, Grafana, Prometheus."
 
@@ -25,21 +25,21 @@ export default {
     id: 'cardiff',
     fromDate: 'Jun, 2012',
     toDate: 'Aug, 2014',
-    place: "Cardiff University, Cardiff, United Kingdom",
+    place: "Cardiff University, Cardiff, UK",
     title: 'Research Fellow in Psychiatric Genetics',
     description: "Genetic and genomics research into adult psychiatric disorders. Designed, implemented, tested and deployed a suite of data processing, analysis and visualisation tools for various data types, in a high throughput distributed environment. Technologies used: R, python (django, numpy, scipy, pandas), javascript (jquery, jquery-ui), databases (postgres, MongoDB), Git.",
   }, {
     id: 'phd',
     fromDate: 'Sep, 2009',
     toDate: 'Jun, 2012',
-    place: "Queen's University, Belfast, United Kingdom",
+    place: "Queen's University, Belfast, UK",
     title: 'PhD in Human Genetics and Bioinformatics',
     description: "Genetic and genomics research into diabetic kidney disease. Designed, implemented, tested and deployed a suite of data processing, analysis and visualisation tools for various data types, in a high throughput distributed environment. Technologies used: R, python (django, numpy, scipy, pandas), javascript (jquery, jquery-ui), databases (postgres, MongoDB), Git.",
   }, {
     id: 'msc',
     fromDate: 'Sep, 2008',
     toDate: 'Sep, 2009',
-    place: "Queen's University, Belfast, United Kingdom",
+    place: "Queen's University, Belfast, UK",
     title: 'MSc in Molecular Medicine',
     description: 'The Masters in Molecular Medicine focused on human disease genetics, genetic epidemiology, bioinformatics, and modern sequencing and genotyping techniques.',
   }, {
